3b7b76a126d608eaea3f9692fd9e1c0c957a9eca
Thamara Leany Machado <106841849+thathamara@users.noreply.github.com>
2022-10-05 21:14:15 -0300

    Add files via upload
    atualização do cod1

--- aula13map/script.js
+++ aula13map/script.js
@@ -1,21 +1,22 @@
 let elementoNum=document.getElementById('inp-num');
 let texx=document.getElementById('texto');
-let valorTexx=texx.value;
 let arr=[];
 let elementoCalc=document.getElementById('btn-calcular');
 let elementoLimp=document.getElementById('btn-limpar');
 
 
 let dado={
-    numero: valorTexx,
+    numero: elementoNum,
 };
 arr.push(dado);
 
-let newArr=dado.map(function( elem ){
-    return{
-        numero: elem.numero*3
+let newArr = dado.map(function (item) {
+    return {
+        nome: item.numero*3
     }
-})
+});
+
+elementoCalc.addEventListener("click",newArr);
 console.log(newArr)
 texx.innerHTML=newArr;
 
